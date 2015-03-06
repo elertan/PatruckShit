80decd9a9c4e6a8737a6cb36c7caea8fb224220a
elertan <denniskievits@l1306099.ssc.local>
2015-03-06 13:44:58 +0100

    Changes

--- Les1/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Les1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 namespace ConsoleApplication1
 {
 
@@ -7,15 +8,32 @@ namespace ConsoleApplication1
         public static double PI = 3.14;
     }
 
-    public class Circle
+    public class Circle : Shape
     {
+        public double Straal;
 
+        public Circle(double Straal)
+        {
+            this.Straal = Straal;
+        }
+
+        public double GetArea()
+        {
+            return this.Straal * this.Straal * Circle.PI;
+        }
     }
 
     public class Program
     {
         static void Main(string[] args)
         {
+            Circle circle1 = new Circle(2.5);
+            Circle circle2 = new Circle(2);
+
+            Console.WriteLine(circle1.GetArea());
+            Console.WriteLine(circle2.GetArea());
+
+            Console.ReadKey();
 
         }
     }
